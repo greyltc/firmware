e582bd9bc0b122c0145797a6b383c147da7863b7
Grey Christoforo <grey@christoforo.net>
2017-02-18 21:32:59 +0000

    cleanup testbed

--- testBed/testBed.ino
+++ testBed/testBed.ino
@@ -4,27 +4,16 @@
 // 8574  Address range is 0x20-0x27
 // 8574A Address range is 0x38-0x3F
 
-
-//#define INaddr 0x3A  // 8574A addr 000
-#define OUTaddr 0x3A // 8574A addr 001
-
-//SPISettings settingsA(2000000, MSBFIRST, SPI_MODE1); 
+#define ADDR_8574A 0x3A // 8574A addr 010
 
 void setup()
 {
   //pinMode(12, INPUT);  // to read /INT
   pinMode(13, OUTPUT); // to show we are working
   Wire.begin();
-  delay(10);
-  expanderWrite(OUTaddr, 0x01); // deselect the switches
-  delay(10);
-  //SPI.beginTransaction(SPISettings(100000, MSBFIRST, SPI_MODE0)); // start the SPI library
-  //SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
-  SPI.begin();
-  //SPISettings settingsA(2000000, MSBFIRST, SPI_MODE1); 
+  expanderWrite(ADDR_8574A, 0x01); // deselect the switches
+  SPI.begin(); // start the SPI library
   SPI.beginTransaction(SPISettings(20000, MSBFIRST, SPI_MODE0));
-  delay(10);
-  //setSwitchState(0x00); // open all the switches
 } 
 
 void expanderWrite(int i2caddr, byte data)
@@ -35,15 +24,9 @@ void expanderWrite(int i2caddr, byte data)
 }
 
 void setSwitchState(byte value) {
-
-  expanderWrite(OUTaddr, 0x00); //cs enable
-  //delay(10);
-
+  expanderWrite(ADDR_8574A, 0x00); //cs enable
   SPI.transfer(value);  //Send value to record into register
-  //delay(10);
-
-  expanderWrite(OUTaddr, 0x01); //cs disable
-  //delay(10);
+  expanderWrite(ADDR_8574A, 0x01); //cs disable
 }
 
 
@@ -55,61 +38,31 @@ void loop()
   delay(50);  // Flash my bling
   digitalWrite(13, 0);
 
-  
-  //int justGnd = 0x81;
-  //int dev0 = 0x83;
-  //int dev1 = 0x85;
-  //int dev2 = 0x89;
-  //int dev3 = 0x91;
-  //int dev4 = 0xA1;
-  //int dev5 = 0xC1;
-
-  expanderWrite(OUTaddr, 0x00); //cs enable
-  SPI.transfer(0xff);  //Send value to record into register
-  expanderWrite(OUTaddr, 0x01); //cs disable
+  setSwitchState(0x81); // only grounds
+  delay(100);
+  setSwitchState(0x83); // dev 0 + gnd
+  delay(100);
+  setSwitchState(0x85); // dev 1 + gnd
+  delay(100);
+  setSwitchState(0x89); // dev 2 + gnd
+  delay(100);
+  setSwitchState(0x91); // dev 3 + gnd
+  delay(100);
+  setSwitchState(0xA1); // dev 4 + gnd
+  delay(100);
+  setSwitchState(0xC1); // dev 5 + gnd
+  delay(100);
+  setSwitchState(0xA1); // dev 4 + gnd
+  delay(100);
+  setSwitchState(0x91); // dev 3 + gnd
+  delay(100);
+  setSwitchState(0x89); // dev 2 + gnd
+  delay(100);
+  setSwitchState(0x85); // dev 1 + gnd
+  delay(100);
+  setSwitchState(0x83); // dev 0 + gnd
+  delay(100);
+  setSwitchState(0x00); // all off
   delay(1000);
-
-  expanderWrite(OUTaddr, 0x00); //cs enable
-  SPI.transfer(0x00);  //Send value to record into register
-  expanderWrite(OUTaddr, 0x01); //cs disable
-
-  
-  //expanderWrite(OUTaddr, 0x01); //cs enable
-  //delay(10);
-  //setSwitchState(0x81); //only grounds enabled
-  //delay(1000);
-  //setSwitchState(0x83); //dev1 + gnd
-  //delay(1000);
-  //setSwitchState(0x85); //dev2 + gnd
-  //delay(1000);
-  //setSwitchState(0x88); //dev3 + gnd
-  //delay(1000);
-  //setSwitchState(0x91); //dev4 + gnd
-  //delay(1000);
-  //setSwitchState(0xA1); //dev5 + gnd
-  //delay(1000);
-  //setSwitchState(0xC1); //dev6 + gnd
-  //delay(1000);
-  //setSwitchState(0x00); //all switches open
-  //delay(1000);
-
-
-  //int justGnd = 0xff;
-  //int dataB = 0x00;
-  
-  //expanderWrite(OUTaddr, 0xff);
-  //delay(1000);
-  //expanderWrite(OUTaddr, 0x00);
-  //delay(1000);
-  //if (data != -1) {
-  //    expanderWrite(OUTaddr, (byte)data);
-  //} else {
-  //    for (int x = 0; x <= 8; x++) { // flash error pattern
-  //      digitalWrite(13, 1); delay(100); 
-  //      digitalWrite(13, 0); delay(400); 
-  //      digitalWrite(13, 1); delay(400); 
-  //      digitalWrite(13, 0); delay(100); 
-  //    }
-  //}
 }
 
