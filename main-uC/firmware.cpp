0f5efcd4aea1a7568ff60262d17094accbd460ae
Greyson Christoforo <grey@christoforo.net>
2020-07-07 19:11:16 +0100

    update build hash

--- main-uC/firmware.cpp
+++ main-uC/firmware.cpp
@@ -1,7 +1,7 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
 #define VERSION_PATCH 1
-#define BUILD a843d30
+#define BUILD d44d9f3
 // ====== start user editable config ======
 
 // when I2C_MUX is defined, we control the pixel selection multiplexer via I2C a la Otter
